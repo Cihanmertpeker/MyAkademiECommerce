cdc726969e43f329f0e35f7e888727dde31a2849
Cihan Mert PEKER <119748061+Cihanmertpeker@users.noreply.github.com>
2024-04-25 11:53:31 +0300

    commit

---
+++ Services/Order/Core/MyAkademiECommerce.Order.Application/Features/Mediator/Handlers/CreateOrderingCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using MyAkademiECommerce.Order.Application.Features.Mediator.Commands;
+using MyAkademiECommerce.Order.Application.Interfaces;
+using MyAkademiECommerce.Order.Domain.Entities;
+
+namespace MyAkademiECommerce.Order.Application.Features.Mediator.Handlers
+{
+    public class CreateOrderingCommandHandler : IRequestHandler<CreateOrderingCommand>
+    {
+        private readonly IRepository<Ordering> _repository;
+
+        public CreateOrderingCommandHandler(IRepository<Ordering> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateOrderingCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new Ordering
+            {
+                OrderDate = request.OrderDate,
+                TotalPrice = request.TotalPrice,
+                UserID = request.UserID,
+            });
+        }
+       
+    }
+}
---
+++ Services/Order/Core/MyAkademiECommerce.Order.Application/Features/Mediator/Handlers/RemoveOrderingCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using MyAkademiECommerce.Order.Application.Features.Mediator.Commands;
+using MyAkademiECommerce.Order.Application.Interfaces;
+using MyAkademiECommerce.Order.Domain.Entities;
+
+namespace MyAkademiECommerce.Order.Application.Features.Mediator.Handlers
+{
+    public class RemoveOrderingCommandHandler : IRequestHandler<RemoveOrderingCommand>
+    {
+        private readonly IRepository<Ordering> _repository;
+
+        public RemoveOrderingCommandHandler(IRepository<Ordering> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveOrderingCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.DeleteAsync(request.Id);
+        }
+    }
+}
